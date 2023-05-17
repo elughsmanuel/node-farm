1a86003e76169a091e1eb49923dd870cbdbafc17
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2023-05-17 15:36:15 +0100

    Parsing Variables from URL

--- index.js
+++ index.js
@@ -52,11 +52,12 @@ const tempProduct = fs.readFileSync(`${__dirname}/././templates/template-product
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const server = http.createServer((req, res) => {        
-    const pathName = req.url;
+const server = http.createServer((req, res) => {     
+    
+    const { query, pathname } = url.parse(req.url, true);
 
     // Overview page
-    if(pathName === '/' || pathName === '/overview') {
+    if(pathname === '/' || pathname === '/overview') {
         res.writeHead(200, { 'Content-type': 'text/html' });
 
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
@@ -65,12 +66,15 @@ const server = http.createServer((req, res) => {
     }
 
     // Product page
-    else if(pathName === '/product') {
-        res.end('This is the PRODUCT');
+    else if(pathname === '/product') {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
     }
 
     // API
-    else if(pathName === '/api') {
+    else if(pathname === '/api') {
             res.writeHead(200, { 'Content-type': 'application/json' });
             res.end(data);        
     }
